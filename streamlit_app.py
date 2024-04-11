0c1ab3c6e75f823b0427a1e7f852d63b9bb6c37a
anju1110 <166722685+anju1110@users.noreply.github.com>
2024-04-12 00:29:01 +0530

    Update streamlit_app.py
    more

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ st.write('The name on your smoothie will be:', name_on_order)
 from snowflake.snowpark.functions import col
 cnx=st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME').col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 #Convert the snowpark into Pandas dataframe to use loc function
@@ -40,7 +40,7 @@ if ingredients_list:
     for  f in ingredients_list:
         ingredients_string+=f+ ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == f, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.write('The search value for ', f,' is ', search_on, '.')
         
         st.subheader(f + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
