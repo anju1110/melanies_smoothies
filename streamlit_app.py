65952db0b2a8bc172b1717c8e4c25f6dd5fc8868
anju1110 <166722685+anju1110@users.noreply.github.com>
2024-04-11 23:43:54 +0530

    Update streamlit_app.py
    some more

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,8 @@ if ingredients_list:
     ingredients_string=''
     for  f in ingredients_list:
         ingredients_string+=f+ ' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(f = 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/f")
         #st.text(fruityvice_response.json())
         fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     #st.write(ingredients_string)
