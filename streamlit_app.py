d075b68e1fa63224d09543f2ee9e8aece8523cde
anju1110 <166722685+anju1110@users.noreply.github.com>
2024-04-11 23:41:26 +0530

    Update streamlit_app.py
    added some more changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,9 @@ if ingredients_list:
     ingredients_string=''
     for  f in ingredients_list:
         ingredients_string+=f+ ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        #st.text(fruityvice_response.json())
+        fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 
     my_insert_stmt="""insert into smoothies.public.orders(ingredients,name_on_order ) values('""" + ingredients_string + """', '""" + name_on_order + """')"""
@@ -43,6 +46,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered,{name_on_order}!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# #st.text(fruityvice_response.json())
+# fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
